fdb77beb3879e6ad3801dfaf633a0282f78bb9dc
imhvit <yosifblancotafur2006@gmail.com>
2026-06-13 11:49:03 -0500

    feat: crear ProductRequest para validación de productos y mejorar respuestas en ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        //
+        $products = Product::all();
+        return response()->json([
+            'success' => true,
+            'data' => $products
+        ]);
     }
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = Product::create($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Producto creado correctamente',
+            'data' => $product
+        ]);
+    }
+    public function show(string $id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json([
+            'success' => true,
+            'data' => $product
+        ]);
     }
     public function update(Request $request, string $id)
     {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,7 @@ class Product extends Model
         'name',
         'description',
         'price',
+        'stock',
         'category_id',
     ];
 
