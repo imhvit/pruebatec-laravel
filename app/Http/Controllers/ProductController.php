566e1f51b0aab83cab0c88c0768f117132717949
imhvit <yosifblancotafur2006@gmail.com>
2026-06-13 10:46:34 -0500

    feat: agregar controladores de categoría y producto

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+    public function destroy(string $id)
+    {
+        //
+    }
+}
