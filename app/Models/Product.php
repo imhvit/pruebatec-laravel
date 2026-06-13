d9d7b29d4b24133bc9e96c011b49a439670724df
imhvit <yosifblancotafur2006@gmail.com>
2026-06-13 12:42:37 -0500

    feat: agregar registro de actividad para el modelo Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Product extends Model
 {
+    use LogsActivity;
     protected $fillable = [
         'name',
         'description',
@@ -17,5 +20,15 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('product')
+            ->setDescriptionForEvent(fn(string $eventName) => "Producto  {$eventName}")
+            ->logOnly(['name', 'text', 'price', 'stock', 'category_id'])
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs();
     }
 }
