4a445d915b2c5804172fba49a489210e1df8e962
imhvit <yosifblancotafur2006@gmail.com>
2026-06-13 11:48:55 -0500

    feat: agregar columna de stock en la tabla de productos y actualizar seeder con valores de stock

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,11 +14,11 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         Product::insert([
-            ['name' => 'Laptop', 'description' => 'Laptop de alta gama', 'price' => 1500.00, 'category_id' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Smartphone', 'description' => 'Smartphone con cámara de alta resolución', 'price' => 800.00, 'category_id' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Bicicleta', 'description' => 'Bicicleta de montaña', 'price' => 500.00, 'category_id' => 3, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Camiseta Deportiva', 'description' => 'Camiseta transpirable para deportes', 'price' => 30.00, 'category_id' => 3, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Libro de Negocios', 'description' => 'Libro sobre estrategias de negocios', 'price' => 20.00, 'category_id' => 5, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Laptop', 'description' => 'Laptop de alta gama', 'price' => 1500.00, 'stock' => 10, 'category_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Smartphone', 'description' => 'Smartphone con cámara de alta resolución', 'price' => 800.00, 'stock' => 20, 'category_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Bicicleta', 'description' => 'Bicicleta de montaña', 'price' => 500.00, 'stock' => 5, 'category_id' => 3, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Camiseta Deportiva', 'description' => 'Camiseta transpirable para deportes', 'price' => 30.00, 'stock' => 15, 'category_id' => 3, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Libro de Negocios', 'description' => 'Libro sobre estrategias de negocios', 'price' => 20.00, 'stock' => 8, 'category_id' => 5, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
