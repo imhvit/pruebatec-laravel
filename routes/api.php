cd14a3107a95dab81537d5135c5d7c488afc19d7
imhvit <yosifblancotafur2006@gmail.com>
2026-06-13 11:48:40 -0500

    feat: agregar configuración de Telescope y mejorar manejo de excepciones en la API

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
